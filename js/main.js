9a6d8df9adfd4203fb5e227c08dc872193336465
Tey-Jo <taehee.jo.88@gmail.com>
2021-01-01 15:06:08 +0100

    layout_done
    tablet+PC
    Mobile needs to be done again

--- js/main.js
+++ js/main.js
@@ -118,8 +118,6 @@
         messageB: document.querySelector('#scroll-section-2 .b'),
         messageC: document.querySelector('#scroll-section-2 .c'),
         messageC: document.querySelector('#scroll-section-2 .d'),
-        // pinB: document.querySelector('#scroll-section-2 .b .pin'),
-        // pinC: document.querySelector('#scroll-section-2 .c .pin'),
         canvas: document.querySelector('#video-canvas-2'),
         context: document.querySelector('#video-canvas-2').getContext('2d'),
         videoImages: []
@@ -141,7 +139,7 @@
         }],
         messageB_opacity_in: [0, 1, {
           start: 0.3,
-          end: 0.4
+          end: 0.35
         }],
         messageC_opacity_in: [0, 1, {
           start: 0.5,
@@ -157,7 +155,7 @@
         }],
         messageB_translateY_in: [20, 0, {
           start: 0.3,
-          end: 0.4
+          end: 0.35
         }],
         messageC_translateY_in: [20, 0, {
           start: 0.5,
@@ -168,35 +166,35 @@
           end: 0.8
         }],
         messageA_opacity_out: [1, 0, {
-          start: 0.25,
+          start: 0.2,
           end: 0.3
         }],
         messageB_opacity_out: [1, 0, {
-          start: 0.45,
-          end: 0.5
+          start: 0.4,
+          end: 0.45
         }],
         messageC_opacity_out: [1, 0, {
-          start: 0.65,
+          start: 0.6,
           end: 0.7
         }],
         messageD_opacity_out: [1, 0, {
-          start: 0.85,
+          start: 0.8,
           end: 0.9
         }],
         messageA_translateY_out: [0, -20, {
-          start: 0.25,
+          start: 0.2,
           end: 0.3
         }],
         messageB_translateY_out: [0, -20, {
-          start: 0.45,
-          end: 0.5
+          start: 0.4,
+          end: 0.45
         }],
         messageC_translateY_out: [0, -20, {
-          start: 0.65,
+          start: 0.6,
           end: 0.7
         }],
         messageD_translateY_out: [0, -20, {
-          start: 0.85,
+          start: 0.8,
           end: 0.9
         }]
       }
@@ -259,35 +257,35 @@
           end: 0.8
         }],
         messageA_opacity_out2: [1, 0, {
-          start: 0.25,
+          start: 0.2,
           end: 0.3
         }],
         messageB_opacity_out2: [1, 0, {
-          start: 0.45,
+          start: 0.4,
           end: 0.5
         }],
         messageC_opacity_out2: [1, 0, {
-          start: 0.65,
+          start: 0.6,
           end: 0.7
         }],
         messageD_opacity_out2: [1, 0, {
-          start: 0.85,
+          start: 0.8,
           end: 0.9
         }],
         messageA_translateY_out2: [0, -20, {
-          start: 0.25,
+          start: 0.2,
           end: 0.3
         }],
         messageB_translateY_out2: [0, -20, {
-          start: 0.45,
+          start: 0.4,
           end: 0.5
         }],
         messageC_translateY_out2: [0, -20, {
-          start: 0.65,
+          start: 0.6,
           end: 0.7
         }],
         messageD_translateY_out2: [0, -20, {
-          start: 0.85,
+          start: 0.8,
           end: 0.9
         }]
       }
@@ -776,14 +774,14 @@
       }
     }
 
-    // 일부 기기에서 페이지 끝으로 고속 이동하면 body id가 제대로 인식 안되는 경우를 해결
-    // 페이지 맨 위로 갈 경우: scrollLoop와 첫 scene의 기본 캔버스 그리기 수행
+    
+    // if go to page top : scrollLoop & first scene drawing
     if (delayedYOffset < 1) {
       scrollLoop();
       sceneInfo[0].objs.canvas.style.opacity = 1;
       sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
     }
-    // 페이지 맨 아래로 갈 경우: 마지막 섹션은 스크롤 계산으로 위치 및 크기를 결정해야할 요소들이 많아서 1픽셀을 움직여주는 것으로 해결
+    // if go to page end : move 1 pixel for recalculate
     if ((document.body.offsetHeight - window.innerHeight) - delayedYOffset < 1) {
       let tempYOffset = yOffset;
       scrollTo(0, tempYOffset - 1);
@@ -800,12 +798,12 @@
 
 
   window.addEventListener('load', () => {
-    setLayout(); // 중간에 새로고침 시, 콘텐츠 양에 따라 높이 계산에 오차가 발생하는 경우를 방지하기 위해 before-load 클래스 제거 전에도 확실하게 높이를 세팅하도록 한번 더 실행
+    setLayout(); // for page refresh (set height)
     document.body.classList.remove('before-load');
     setLayout();
     sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
 
-    // 중간에서 새로고침 했을 경우 자동 스크롤로 제대로 그려주기
+    // for page refresh in the middle 
     let tempYOffset = yOffset;
     let tempScrollCount = 0;
     if (tempYOffset > 0) {
@@ -837,15 +835,15 @@
     });
 
     window.addEventListener('orientationchange', () => {
-      scrollTo(0, 0);
+      scrollTo(0, 0);c
       setTimeout(() => {
         window.location.reload();
       }, 500);
     });
 
-    // document.querySelector('.loading').addEventListener('transitionend', (e) => {
-    //   document.body.removeChild(e.currentTarget);
-    // });
+    document.querySelector('.loading').addEventListener('transitionend', (e) => {
+      document.body.removeChild(e.currentTarget);
+    });
 
   });
 
